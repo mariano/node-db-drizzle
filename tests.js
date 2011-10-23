2fba61e3d5cbb952321f0c3268f5dd4bef3ad4d8
Mariano Iglesias <mariano@cricava.com>
2011-10-23 13:54:44 -0300

    Allowing users to change precision. Fixing and adding tests

--- tests.js
+++ tests.js
@@ -1,8 +1,15 @@
 /* Escape & Query building tests */
 
+var settings = JSON.parse(require('fs').readFileSync('./tests-settings.json','utf8'));
+
 var drizzle = require("./db-drizzle");
-var tests = require("./lib/node-db/tests.js").get(function() {
-    return new drizzle.Database();
+var tests = require("./lib/node-db/tests.js").get(function(callback) {
+    new drizzle.Database(settings).connect(function(err) {
+        if (err) {
+            throw new Error('Could not connect to test DB');
+        }
+        callback(this);
+    });
 });
 
 for(var test in tests) {
